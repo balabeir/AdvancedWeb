7e514a485eec7a49f135795ead27ac11f73b6a9b
balabeir <beer32853@gmail.com>
2021-01-12 10:58:19 +0700

    hello cloud

--- HelloCloud.py
+++ HelloCloud.py
@@ -3,7 +3,7 @@ server = Flask(__name__)
 
 @server.route("/")
 def hello():
-    return "hello form server"
+    return "hello from server"
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port="80")

6c4702d94eabf00ef4bb1947a71fe9b41501f8c8
balabeir <beer32853@gmail.com>
2021-01-29 21:08:58 -0800

    a

--- Lecture-03/ConnectDB.py
+++ Lecture-03/ConnectDB.py
@@ -3,16 +3,16 @@ try:
     connection = psycopg2.connect(user="webadmin",
                                   password="HAHebv30768",
                                   host="10.100.2.159",
-                                  #host="node8563-advweb-07.app.ruk-com.cloud",
+                                  # host="node8563-advweb-07.app.ruk-com.cloud",
                                   port="5432",
-                                  #port="11072",
+                                  # port="11072",
                                   database="cloudDB")
 
     cursor = connection.cursor()
-    #print PostgreSQL Connection properties
-    print(connection.get_dsn_parameters(),"\n")
+    # print PostgreSQL Connection properties
+    print(connection.get_dsn_parameters(), "\n")
 
-    #print postgreSQL version
+    # print postgreSQL version
     cursor.execute("SELECT version();")
     record = cursor.fetchone()
     print("You are connected to - ", record, "\n")
@@ -21,7 +21,7 @@ except (Exception, psycopg2.Error) as error:
     print("Error while connecting to PostgreSQL", error)
 
 finally:
-    #closing database connection.
+    # closing database connection.
     if(connection):
         cursor.close()
         connection.close()
